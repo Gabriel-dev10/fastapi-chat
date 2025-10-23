c6710ed128dab92df4ec735692c0087a81f1fd08
Gabriel Santana <gabrielssantana41@gmail.com>
2025-10-23 13:20:09 -0300

    Revert "adiceadiciona div - index.html"
    This reverts commit f36ff6893a093f7cdaf7cb30509a63645cecd4a4.

--- fastapi/app/routes/messages.py
+++ fastapi/app/routes/messages.py
@@ -8,8 +8,6 @@ from bson import ObjectId
 router = APIRouter()
 ws_manager = WSManager()
 
-
-
 @router.get("/messages", response_model=List[MessageOut])
 def read_messages():
     return get_messages()
